5c7dbe490d1ec9f5dad26f338ec05f84e1298c40
LTo28 <to.larry92@gmail.com>
2019-05-20 21:28:18 -0700

    added orm and updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,12 @@
-DROP DATABASE IF EXISTS burgers_db;
-CREATE DATABASE burgers_db;
+CREATE DATABASE IF NOT EXISTS burgers_db;
 
 USE burgers_db;
 
+DROP TABLE IF EXISTS burgers;
+
 CREATE TABLE burgers (
 	id INT AUTO_INCREMENT NOT NULL,
-  burger_name VARCHAR(50) NOT NULL,
-  devoured BOOL NOT NULL,
+  burger_name VARCHAR(255) NOT NULL,
+  devoured BOOLEAN DEFAULT FALSE NOT NULL,
   PRIMARY KEY (id)
 );
